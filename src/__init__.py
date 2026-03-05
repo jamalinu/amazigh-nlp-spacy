23461e666bea43dca37d46395e1d1f427737fe87
Jamal Saghraoui <66197476+jamalinu@users.noreply.github.com>
2026-03-05 12:15:46 +0100

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+from .tamazight_ner import TamazightNER
+from .sentiment import TamazightSentiment
+from .tokenizer import TamazightTokenizer
+
+__version__ = "0.3.0"
+__author__  = "Jamal Saghraoui"
