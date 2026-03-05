1080f4dd3dcefdb5c93d71848c60fa94d530fda2
Jamal Saghraoui <66197476+jamalinu@users.noreply.github.com>
2026-03-05 14:40:02 +0100

    Delete src/amazigh_nlp directory

--- src/amazigh_nlp/__init__.py
+++
@@ -1,6 +0,0 @@
-from .tamazight_ner import TamazightNER
-from .sentiment import TamazightSentiment
-from .tokenizer import TamazightTokenizer
-
-__version__ = "0.3.0"
-__author__  = "Jamal Saghraoui"
